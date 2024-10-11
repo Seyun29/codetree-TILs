42be44147a4c0ad7822fdb0c81a3b4a311e2603b
Seyun Jang <45298516+Seyun29@users.noreply.github.com>
2024-10-11 15:15:37 +0900

    Title: 규칙에 따라 밀기, Time: 104ms, Memory: 24MB, Status: Passed - CodeTree

--- 241011/규칙에 따라 밀기/push-by-the-rules.py
+++ 241011/규칙에 따라 밀기/push-by-the-rules.py
@@ -1,11 +1,9 @@
 a,b=input(),input()
 ans=a
-l,r=b.count('L'),b.count('R')
-if(l>r):
-    for i in range(l-r):
+for d in b:
+    if d=="L":
         ans=ans[1:]+ans[0]
-elif(r>l):
-    for i in range(l-r):
+    else:
         ans=ans[-1]+ans[:-1]
 
 print(ans)
